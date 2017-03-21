31ec58d912da61cd63fecb1ad7c8b23c39a84769
Countzero <git@numerizen.com>
2017-03-21 11:16:58 +0100

    Added menu items for collections and files on admin page.

--- views/admin/page/index.php
+++ views/admin/page/index.php
@@ -2,6 +2,9 @@
 echo head(array('title' => "UI Templates $type page"));
 echo flash(); 
 ?>
+<a class='add button small green' href='<?php echo WEB_ROOT; ?>/admin/uitemplate/item'>Items</a>
+<a class='add button small green' href='<?php echo WEB_ROOT; ?>/admin/uitemplate/collection'>Collections</a>
+<a class='add button small green' href='<?php echo WEB_ROOT; ?>/admin/uitemplate/file'>Files</a>
 <?php 
 
 echo $form;
